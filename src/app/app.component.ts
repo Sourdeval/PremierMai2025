81571c3f64fa572a5b7d7c36e3ac9fed759dbe09
Sourdeval <sourdeval@laposte.net>
2025-03-14 13:28:21 +0100

    initial commit
    ng new PremierMai2025 --prefix pre-mai25 --routing --no-standalone --ssr=false

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pre-mai25-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'PremierMai2025';
+}
