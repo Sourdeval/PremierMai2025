a4ba2c4b52bdb0863e33a33ff6217a6bc214ba1f
Sourdeval <sourdeval@laposte.net>
2025-03-20 23:20:35 +0100

    [Firebase] Add Angular/fire (firebase for Angular)
    npm install -g firebase-tools
    firebase login
    ng add @angular/fire
    
    Just need to rename the project without uppercase..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,18 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp(
+      {
+        projectId: "premiermai2025b",
+        appId: "1:41369266826:web:e7878cd3717a3dbe581b73",
+        storageBucket: "premiermai2025b.firebasestorage.app",
+        apiKey: "AIzaSyB5fHwEy6YbsyW_Gy9e-JwEgI63v6g6qfc",
+        authDomain: "premiermai2025b.firebaseapp.com",
+        messagingSenderId: "41369266826"
+      })),
+    provideFirestore(() => getFirestore())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
