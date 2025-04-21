e9c028b5857685df59d66eb98ef09a0b9b3d61e1
Sourdeval <sourdeval@laposte.net>
2025-04-22 01:17:20 +0200

    [Firebase] ignore credentials

--- src/app/firebaseCredentials.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseCredentials = {
-  projectId: "premiermai2025b",
-  appId: "1:41369266826:web:e7878cd3717a3dbe581b73",
-  storageBucket: "premiermai2025b.firebasestorage.app",
-  apiKey: "AIzaSyB5fHwEy6YbsyW_Gy9e-JwEgI63v6g6qfc",
-  authDomain: "premiermai2025b.firebaseapp.com",
-  messagingSenderId: "41369266826"
-};
